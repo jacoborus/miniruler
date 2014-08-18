1a0ef5d1fa52c0f325dd26fa10f8cc2525b8f49d
Jacobo Tabernero <jacobo@xarope.eu>
2014-08-17 19:27:07 -0500

    resources are now contexts

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
-var deepExtend = require('deep-extend');
+var ruler;
 
-var contexts = {};
-
+/*!
+ * Add or overwrite roles in context or resource
+ * @param {[type]} roles [description]
+ */
 var setRoles = function (roles) {
 	var i;
 	for (i in roles) {
@@ -14,6 +16,10 @@ var setRoles = function (roles) {
 	}
 };
 
+/*!
+ * Remove roles form context or resource
+ * @param  {Array|String} roles role name or list of role names
+ */
 var removeRoles = function (roles) {
 	var i;
 	if (typeof roles === 'string') {
@@ -24,6 +30,10 @@ var removeRoles = function (roles) {
 	}
 };
 
+/*!
+ * Add or overwrite actions in context or resource
+ * @param {Object} actions actions and its rules
+ */
 var setActions = function (actions) {
 	var i;
 	for (i in actions) {
@@ -31,6 +41,11 @@ var setActions = function (actions) {
 	}
 };
 
+/*!
+ * Remove actions from context or resource
+ * @param  {[type]} actions [description]
+ * @return {[type]}         [description]
+ */
 var removeActions = function (actions) {
 	var i;
 	if (typeof actions === 'string') {
@@ -41,61 +56,122 @@ var removeActions = function (actions) {
 	}
 };
 
-
-var Resource = function (obj) {
-	this.roles = {};
-	this.actions = {};
-	setRoles.call( this, obj.roles );
-	setActions.call( this, obj.actions );
-};
-
-Resource.prototype.setRoles = function (roles) {
-	setRoles.call( this, roles );
-};
-
-Resource.prototype.removeRoles = function (roles) {
-	removeRoles.call( this, roles );
-};
-
-Resource.prototype.setActions = function (actions) {
-	setActions.call( this, actions );
-};
-
-Resource.prototype.removeActions = function (actions) {
-	removeActions.call( this, actions );
-};
-
-Resource.prototype.check = function (action, rol) {
-	// body...
-};
-
 /**
  * Context constructor
  * @param {Object} ctx [description]
  */
-var Context = function (name, ctx) {
+var Context = function (ctx, parent) {
 	var i;
-	this.resources = {};
+	this.contexts = {};
 	this.roles = {};
 	this.actions = {};
 	ctx = ctx || {};
 	setRoles.call( this, ctx.roles  || {});
 	setActions.call( this, ctx.actions  || {});
-	for (i in ctx.resources || {}) {
-		this.resources[i]  = new Resource( ctx.resources[i] );
+	for (i in ctx.contexts || {}) {
+		this.contexts[i]  = new Context( ctx.contexts[i] );
 	}
-	contexts[name] = this;
 };
 
+/**
+ * Add or overwrite a role
+ * @param {String} name role keyname
+ * @param {Number} level level role. Optional, 1 by default
+ */
+Context.prototype.setRole = function (name, level) {
+	if (!name) {
+		throw new Error( 'Role name is required to set a role' );
+	}
+	if (typeof name !== 'string') {
+		throw new Error( 'Role name must be a string' );
+	}
+	if (typeof level !== 'undefined' && typeof level !== 'number') {
+		throw new Error( 'Role level must be a number' );
+	}
+	level = level || 1;
+	this.roles[name] = level;
+};
+
+/**
+ * Add or update roles to a context
+ * @param {Object} roles Roles names and its levels
+ */
 Context.prototype.setRoles = function (roles) {
+	if (!roles || typeof roles !== 'object') {
+		throw new Error( 'setRoles requires a object as param' );
+	}
 	setRoles.call( this, roles );
 };
 
+/**
+ * Remove one or multiple roles from context
+ * @param  {String|Array} roles list of roles to delete
+ */
 Context.prototype.removeRoles = function (roles) {
+	roles = roles || [];
+	if (typeof roles === 'string') {
+		roles = [roles];
+	}
 	removeRoles.call( this, roles );
 };
 
+/**
+ * Add or update action rules
+ * @param {String} name   action keyname
+ * @param {Object} rules action rules object, see [action rules](#action_rules)
+ */
+Context.prototype.setAction = function (name, rules) {
+	if (typeof name !== 'string') {
+		throw new Error( 'Action keyname must be a string' );
+	}
+	if (typeof rules !== 'object') {
+		throw new Error( 'Action rules must be a object' );
+	}
+	var nums = ['level', 'parentLevel', 'mainLevel'],
+		arrs = ['roles', 'parentRoles', 'mainRoles'],
+		i;
+
+	rules = rules || {};
+	this.actions[name] = {};
+
+	for (i in nums) {
+		if (typeof rules[nums[i]] !== 'undefined') {
+			if (typeof rules[nums[i]] !== 'number' && rules[nums[i]] !== null ) {
+				throw new Error( nums[i] + ' must be an number or null');
+			}
+			if (rules[nums[i]] === null) {
+				this.actions[name][nums[i]] = null;
+			} else {
+				this.actions[name][nums[i]] = rules[nums[i]];
+			}
+		} else {
+			this.actions[name][nums[i]] = this.actions[name][nums[i]] || [];
+		}
+	}
+	for (i in arrs) {
+		if (typeof rules[arrs[i]] !== 'undefined') {
+			if (typeof rules[arrs[i]] !== 'object') {
+				throw new Error( arrs[i] + ' must be an array');
+			}
+			if (rules[arrs[i]] === null) {
+				this.actions[name][arrs[i]] = [];
+			} else {
+				this.actions[name][arrs[i]] = rules[arrs[i]];
+			}
+		} else {
+			this.actions[name][arrs[i]] = this.actions[name][arrs[i]] || [];
+		}
+	}
+};
+
+/**
+ * Add or update actions in context
+ * @param {Object} actions action rules
+ */
 Context.prototype.setActions = function (actions) {
+	if (typeof actions !== 'object') {
+		throw new Error( 'Action rules must be a object' );
+	}
 	setActions.call( this, actions );
 };
 
@@ -103,20 +179,17 @@ Context.prototype.removeActions = function (actions) {
 	removeActions.call( this, actions );
 };
 
-Context.prototype.setResource = function (name, obj) {
-	this.resources[name] = !this.resources[name] ? new Resource( obj ) : deepExtend( this.resources[name], obj );
+Context.prototype.addContext = function (name, obj) {
+	if (this.contexts[name]) {
+		throw Error;
+	}
+	this.contexts[name] = new Context( obj, this );
 };
 
-Context.prototype.removeResource = function (name) {
+Context.prototype.removeContext = function (name) {
 	delete this.resources[name];
 };
 
-Context.prototype.check = function (resource, action, role) {
-	// body...
-};
+ruler = new Context();
 
-
-module.exports = {
-	Context: Context,
-	contexts: contexts
-};
+module.exports = ruler;
--- test/index.js
+++ test/index.js
@@ -3,20 +3,178 @@
 var expect = require('chai').expect,
 	ruler = require('..');
 
-describe( 'Ruler constructor', function () {
-
-	var context = new ruler.Context( 'uno' );
-	it( 'add itself to ruler.contexts', function () {
-		expect( ruler.contexts.uno ).to.exist;
-		expect( ruler.contexts.uno ).to.be.a('object');
-	});
+describe( 'miniruler', function () {
 
 	it( 'has correct structure', function () {
-		expect( ruler.contexts.uno.roles ).to.exist;
-		expect( ruler.contexts.uno.roles ).to.be.a('object');
-		expect( ruler.contexts.uno.actions ).to.exist;
-		expect( ruler.contexts.uno.actions ).to.be.a('object');
-		expect( ruler.contexts.uno.resources ).to.exist;
-		expect( ruler.contexts.uno.resources ).to.be.a('object');
+		expect( ruler.roles ).to.exist;
+		expect( ruler.roles ).to.be.a('object');
+		expect( ruler.actions ).to.exist;
+		expect( ruler.actions ).to.be.a('object');
+		expect( ruler.contexts ).to.exist;
+		expect( ruler.contexts ).to.be.a('object');
+	});
+});
+
+
+describe( 'setRole', function () {
+
+	it( 'requires a string as keyname', function () {
+		expect( function () {
+			ruler.setRole( );
+		}).to.throw( 'Role name is required to set a role' );
+
+		expect( function () {
+			ruler.setRole( 1 );
+		}).to.throw( Error );
+	});
+
+	it( 'requires a number as level', function () {
+		expect( function () {
+			ruler.setRole( 1 );
+		}).to.throw( Error );
+	});
+
+	ruler.setRole( 'uno' );
+
+	it( 'set role with its name in its context.roles', function () {
+		expect( ruler.roles.uno ).to.exist;
+	});
+
+	it( 'add 1 as default level', function () {
+		expect( ruler.roles.uno ).to.equal(1);
+	});
+
+	it( 'add role with its level', function () {
+		ruler.setRole( 'dos', 2 );
+		expect( ruler.roles.dos ).to.equal(2);
+	});
+});
+
+
+describe( 'setRoles', function () {
+
+	it( 'requires a object as param', function () {
+		expect( function () {
+			ruler.setRoles( );
+		}).to.throw( 'setRoles requires a object as param' );
+
+		expect( function () {
+			ruler.setRoles( 2 );
+		}).to.throw( 'setRoles requires a object as param' );
+	});
+
+	it( 'set roles from object', function () {
+		var roles = {
+			admin: 5,
+			autor: 3,
+			member: 1
+		};
+		ruler.setRoles( roles );
+		expect( ruler.roles.admin ).to.equal(5);
+	});
+});
+
+
+describe( 'removeRoles', function () {
+
+	it( 'remove a single role', function () {
+		ruler.setRole( 'testsinglerole', 3 );
+		ruler.removeRoles( 'testsinglerole' );
+		expect( ruler.roles.testsinglerole ).to.not.exist;
+	});
+
+	it( 'remove multiple roles', function () {
+		ruler.setRole( 'multirol1', 3 );
+		ruler.setRole( 'multirol2', 3 );
+		ruler.removeRoles([ 'multirol1', 'multirol2' ]);
+		expect( ruler.roles.multirol1 ).to.not.exist;
+		expect( ruler.roles.multirol2 ).to.not.exist;
+	});
+});
+
+describe( 'setAction', function () {
+
+	it( 'requires a string name param', function () {
+		expect( function () {
+			ruler.setAction( 1 );
+		}).to.throw( 'Action keyname must be a string' );
+	});
+
+	it( 'requires an object as rules param', function () {
+		expect( function () {
+			ruler.setAction( 'tres' );
+		}).to.throw( 'Action rules must be a object' );
+	});
+
+	it( 'add rules to a new action', function () {
+		var rules = {
+			level: 2,
+			roles: ['editor', 'member'],
+			parentLevel: 4,
+			parentRoles: ['owner'],
+			mainLevel: 10,
+			mainRoles: ['superadmin']
+		};
+		ruler.setAction( 'tres', rules );
+		expect( ruler.actions.tres.level ).to.equal(2);
+		expect( ruler.actions.tres.roles[0] ).to.equal('editor');
+		expect( ruler.actions.tres.parentLevel ).to.equal(4);
+		expect( ruler.actions.tres.parentRoles[0] ).to.equal('owner');
+		expect( ruler.actions.tres.mainLevel ).to.equal(10);
+		expect( ruler.actions.tres.mainRoles[0] ).to.equal('superadmin');
+	});
+
+	it( 'remove rule when field is null', function () {
+		var rules = {
+			level: 2,
+			roles: ['editor', 'member'],
+			parentLevel: 4,
+			parentRoles: ['owner'],
+			mainLevel: 10,
+			mainRoles: ['superadmin']
+		};
+		ruler.setAction( 'cuatro', rules );
+		var otherRules = {
+			level: null,
+			roles: null,
+			parentLevel: null,
+			parentRoles: null,
+			mainLevel: null,
+			mainRoles: null
+		};
+		ruler.setAction( 'cuatro', otherRules );
+		expect( ruler.actions.cuatro.level ).to.equal( null );
+		expect( ruler.actions.cuatro.roles[0] ).to.not.exist;
+		expect( ruler.actions.cuatro.parentLevel ).to.equal( null );
+		expect( ruler.actions.cuatro.parentRoles[0] ).to.not.exist;
+		expect( ruler.actions.cuatro.mainLevel ).to.equal( null );
+		expect( ruler.actions.cuatro.mainRoles[0] ).to.not.exist;
+	});
+});
+
+
+describe( 'setActions', function () {
+
+	it( 'requires a object with rules', function () {
+		expect( function () {
+			ruler.setActions( 1 );
+		}).to.throw( 'Action rules must be a object' );
+	});
+
+	it( 'launch setAction for every action', function () {
+		var rules = {
+			level: 2,
+			roles: ['editor', 'member'],
+			parentLevel: 4,
+			parentRoles: ['owner'],
+			mainLevel: 10,
+			mainRoles: ['superadmin']
+		};
+		ruler.setActions({
+			'cinco': rules,
+			'seis': rules
+		});
+		expect( ruler.actions.cinco.level ).to.equal(2);
+		expect( ruler.actions.seis.level ).to.equal(2);
 	});
 });
