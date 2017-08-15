7fe6aeeeb9b6030c464eab2397d4fd03dc38cd85
Jacobo Tabernero <jacobo@frontloopmedia.com>
2017-08-15 20:52:55 +0200

    Update: type checking test, createAction with level

--- test/index.js
+++ test/index.js
@@ -1,30 +1,35 @@
 const test = require('tape')
-const ruler = require('..')
+const r = require('..')
 
 test('createAction: error checking', t => {
   // action name
-  t.throws(() => { ruler.createAction() }, 'action name should be present')
-  t.throws(() => { ruler.createAction(1) }, 'action name should be a string')
-  t.throws(() => { ruler.createAction('') }, 'action name cannot be a empty string')
+  t.throws(() => { r.createAction() }, 'action name should be present')
+  t.throws(() => { r.createAction(1) }, 'action name should be a string')
+  t.throws(() => { r.createAction('') }, 'action name cannot be a empty string')
   // action roles
-  t.throws(() => { ruler.createAction('e', {roles: 1}) }, 'action roles should be an array (num)')
-  t.throws(() => { ruler.createAction('e', {roles: {}}) }, 'action roles should be an array (obj)')
-  t.throws(() => { ruler.createAction('e', {roles: ''}) }, 'action roles should be an array (str)')
+  t.throws(() => { r.createAction('e', {roles: 1}) }, 'action roles should be an array (num)')
+  t.throws(() => { r.createAction('e', {roles: {}}) }, 'action roles should be an array (obj)')
+  t.throws(() => { r.createAction('e', {roles: ''}) }, 'action roles should be an array (str)')
   t.throws(
-    () => { ruler.createAction('e', {roles: [1]}) },
+    () => { r.createAction('e', {roles: [1]}) },
     'action roles should be an array of strings (arr with wrong types inside, numbers)'
   )
   t.throws(
-    () => { ruler.createAction('e', {roles: [{}]}) },
+    () => { r.createAction('e', {roles: [{}]}) },
     'action roles should be an array of strings (arr with wrong types inside, object)'
   )
   t.doesNotThrow(
-    () => { ruler.createAction('r1', {roles: []}) }, 'action roles should be an array (arr)'
+    () => { r.createAction('r1', {roles: []}) }, 'action roles should be an array (arr)'
   )
   t.doesNotThrow(
-    () => { ruler.createAction('r2', {roles: ['role1', 'role2']}) },
+    () => { r.createAction('r2', {roles: ['role1', 'role2']}) },
     'action roles should be an array of strings'
   )
   // action levels
+  t.throws(() => { r.createAction('e', {level: 's'}) }, 'action level should be an number (str)')
+  t.throws(() => { r.createAction('e', {level: {}}) }, 'action level should be an number (obj)')
+  t.throws(() => { r.createAction('e', {level: []}) }, 'action level should be an number (obj)')
+  t.doesNotThrow(() => { r.createAction('rl1', {level: 0}) }, 'action level should be an number')
+  t.doesNotThrow(() => { r.createAction('rl2', {level: 99}) }, 'action level should be an number')
   t.end()
 })
